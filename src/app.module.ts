75eed00c2f64e470dd237742484a01a5a2a2ee8b
nhatndm <nhatndm1193@gmail.com>
2019-08-24 23:30:38 +0700

    added postgress configuration and .env

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import * as OrmConfig from '../ormconfig';
 import { AppService } from './app.service';
 
 @Module({
-  imports: [],
+  imports: [TypeOrmModule.forRoot(OrmConfig)],
   controllers: [AppController],
   providers: [AppService],
 })
